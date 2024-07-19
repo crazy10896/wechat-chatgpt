82dbd5a953f24fb0f9d2c9e5ffd174c8027e538f
crazy10896 <crazy10896@proton.me>
2024-07-19 14:34:18 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -5,9 +5,9 @@ import { IConfig } from "./interface";
 export const config: IConfig = {
   api: 'https://api.chatanywhere.tech/v1',
   openai_api_key: process.env.OPENAI_API_KEY || "123456789",
-  model: process.env.MODEL || "gpt-3.5-turbo",
-  chatPrivateTriggerKeyword: process.env.CHAT_PRIVATE_TRIGGER_KEYWORD || "",
-  chatTriggerRule: process.env.CHAT_TRIGGER_RULE || "",
+  model: process.env.MODEL || "gpt-4o-mini",
+  chatPrivateTriggerKeyword: process.env.CHAT_PRIVATE_TRIGGER_KEYWORD || "bot",
+  chatTriggerRule: process.env.CHAT_TRIGGER_RULE || "bot",
   disableGroupMessage: process.env.DISABLE_GROUP_MESSAGE === "true",
   temperature: process.env.TEMPERATURE ? parseFloat(process.env.TEMPERATURE) : 0.6,
   blockWords: process.env.BLOCK_WORDS?.split(",") || [],
